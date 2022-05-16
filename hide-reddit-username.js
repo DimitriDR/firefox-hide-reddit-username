5bf88261851bd6f4c695e0c87ccca17b3353f292
Leonetienne <leon@etiennes.de>
2022-05-16 13:55:53 +0200

    Fix issue #1, hiding username doesnt always work

--- hide-reddit-username.js
+++ hide-reddit-username.js
@@ -1,2 +1,17 @@
 /* To replace your Reddit username when commenting */
-document.getElementsByClassName("_23wugcdiaj44hdfugIAlnX")[1].textContent = "Snoo";
+function hideUsername() {
+  // There might be multiple elements displaing the username. Hide them all.
+  usernameElements = document.getElementsByClassName("_23wugcdiaj44hdfugIAlnX")
+  for (let i = 0; i < usernameElements.length; i++) {
+    usernameElements[i].textContent = "Whiffkaws"
+  }
+}
+
+// Hide once immediately
+hideUsername();
+
+// Reddit also sometimes shows the username without doing a reload.
+// So lets just attempt to remove it every second. Shouldnt be too much
+// of a performance hog...
+window.setInterval(hideUsername, 1000)
+
