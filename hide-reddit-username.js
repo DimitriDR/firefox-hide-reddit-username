11ed97c370fdcf8387f825a602c41ab1f55a8abb
Leonetienne <leon@etiennes.de>
2022-05-18 21:06:48 +0200

    Now selectively only removing the users username in comments. Not just everyones.

--- hide-reddit-username.js
+++ hide-reddit-username.js
@@ -1,17 +1,32 @@
 /* To replace your Reddit username when commenting */
+
 function hideUsername() {
-  // There might be multiple elements displaing the username. Hide them all.
+
+  // Get the username
+  const username = document.getElementsByClassName("_2BMnTatQ5gjKGK5OWROgaG")[0].textContent;
+
+  // There might be multiple elements displaying the username.
   usernameElements = document.getElementsByClassName("_23wugcdiaj44hdfugIAlnX")
   for (let i = 0; i < usernameElements.length; i++) {
-    usernameElements[i].textContent = "Whiffkaws"
+
+    // Does the element containing a username actually contain "our" user name?
+    if (usernameElements[i].textContent === username) {
+      // Yes: replace it.
+      usernameElements[i].textContent = "Whiffkaws"
+    }
   }
 }
 
-// Hide once immediately
-hideUsername();
+// We should also wait for the website to have finished loading completely.
+// Otherwise it may fail on a very slow internet connection.
+window.addEventListener("load", () => {
 
-// Reddit also sometimes shows the username without doing a reload.
-// So lets just attempt to remove it every second. Shouldnt be too much
-// of a performance hog...
-window.setInterval(hideUsername, 1000)
+  // Hide once immediately
+  hideUsername();
+
+  // Reddit also sometimes adds new user name elements, without reloading the dom.
+  // So lets just attempt to remove them every second. Shouldnt be too much
+  // of a performance hog...
+  window.setInterval(hideUsername, 1000)
+});
 
